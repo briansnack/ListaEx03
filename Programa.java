0bedb178abb16f1c9bd6d5772213c6292c17afb0
Brian Snack <122538036+briansnack@users.noreply.github.com>
2024-04-15 20:49:32 -0300

    Ajuste do Programa.java

--- Programa.java
+++ Programa.java
@@ -1,30 +1,119 @@
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String args[]){
-        Ex01.executar();
-        // Ex02.executar();
-        // Ex03.executar();
-        // Ex04.executar();
-        // Ex05.executar();
-        // Ex06.executar();
-        // Ex07.executar();
-        // Ex08.executar();
-        // Ex09.executar();
-        // Ex10.executar();
-        // Ex11.executar();
-        // Ex12.executar();
-        // Ex13.executar();
-        // Ex14.executar();
-        // Ex15.executar();
-        // Ex16.executar();
-        // Ex17.executar();
-        // Ex18.executar();
-        // Ex19.executar();
-        // Ex20.executar();
-        // Ex21.executar();
-        // Ex22.executar();
-        // Ex23.executar();
-        // Ex24.executar();
-        // Ex25.executar();
-        // Ex26.executar();
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Escolha um exercício: ");
+        int exercicioEscolhido = leitor.nextInt();
+        
+        switch (exercicioEscolhido) {
+            case 1:
+                Ex01.executar();
+                break;
+        
+            case 2:
+                Ex02.executar();
+                break;
+
+            case 3:
+                Ex03.executar();
+                break;
+            
+            case 4:
+                Ex04.executar();    
+                break;
+                
+            case 5:
+                Ex05.executar();    
+                break;
+
+            case 6:
+                Ex06.executar();    
+                break;
+
+            case 7:
+                Ex07.executar();    
+                break;
+
+            case 8:
+                Ex08.executar();    
+                break;
+
+            case 9:
+                Ex09.executar();    
+                break;
+
+            case 10:
+                Ex10.executar();    
+                break;
+
+            case 11:
+                Ex11.executar();    
+                break;
+
+            case 12:
+                Ex12.executar();    
+                break;
+
+            case 13:
+                Ex13.executar();    
+                break;
+
+            case 14:
+                Ex14.executar();    
+                break;
+
+            case 15:
+                Ex15.executar();    
+                break;
+
+            case 16:
+                Ex16.executar();    
+                break;
+
+            case 17:
+                Ex17.executar();    
+                break;
+
+            case 18:
+                Ex18.executar();    
+                break;
+
+            case 19:
+                Ex19.executar();    
+                break;
+
+            case 20:
+                Ex20.executar();    
+                break;
+
+            case 21:
+                Ex21.executar();    
+                break;
+
+            case 22:
+                Ex22.executar();    
+                break;
+
+            case 23:
+                Ex23.executar();    
+                break;
+
+            case 24:
+                Ex24.executar();    
+                break;
+
+            case 25:
+                Ex25.executar();    
+                break;
+
+            case 26:
+                Ex26.executar();    
+                break;
+
+            default:
+            System.out.println("Exercício não reconhecido");
+                break;
+        }
     }
 }
