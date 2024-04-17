bd02c0085d2fc8f762abadea42eab644ff361d84
Brian Snack <122538036+briansnack@users.noreply.github.com>
2024-04-16 22:09:55 -0300

    Exercicio10 com encapsulamento

--- br/edu/up/exercicios/Ex10.java
+++ br/edu/up/exercicios/Ex10.java
@@ -3,13 +3,20 @@ public class Ex10 {
     public static void executar() {
         Scanner leitor = new Scanner(System.in);
         int[] idade = new int[5];
-        Pessoa pessoa = new Pessoa();
 
         for (int i = 0; i < idade.length; i++) {
             System.out.println("Idade da pessoa " + (i+1) + ": ");
             idade[i] = leitor.nextInt();
-            pessoa.verificarIdade(idade[i]);
         }
+        
+        for(int i = 0; i < idade.length; i++){
+            Pessoa pessoa = new Pessoa();
+            pessoa.setIdade(idade[i]);
+            pessoa.verificarIdade();
+        }
+        
+
+
         leitor.close();
     }
 }
--- br/edu/up/exercicios/models/Pessoa.java
+++ br/edu/up/exercicios/models/Pessoa.java
@@ -1,6 +1,10 @@
 class Pessoa {
+    private int idade;
 
-    public void verificarIdade(int idade) {
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public void verificarIdade() {
         if (idade >= 18) {
             System.out.println("Maior de idade");
         } else {
